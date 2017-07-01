839427a41e08f796992300b2cf509586cb5db92c
shail1312 <shail1312@hotmail.com>
2017-07-02 00:44:33 +0530

    Added todos to firebase

--- app/app.jsx
+++ app/app.jsx
@@ -4,14 +4,13 @@ var {Provider} = require('react-redux');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 var TodoApp = require('TodoApp');
-
 var actions = require('actions');
 var store = require('configureStore').configure();
 var TodoAPI = require('TodoAPI');
 
 store.subscribe(() => {
   var state = store.getState();
-  console.log('New state', store.getState());
+  console.log('New state', state);
   TodoAPI.setTodos(state.todos);
 });
 
